c260b86ad887ba8085a46263b3f425bb5e9baaa1
SebastianLoef <sebastian@loef.se>
2023-12-11 16:33:55 +0100

    change module1 to module

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-module1 = Extension(
+module = Extension(
     name="my_c_module",
     sources=["fib/main.c"],
     include_dirs=[],  # list of directories to search for C/C++ header files
@@ -9,4 +9,4 @@ module1 = Extension(
     libraries=[],  # list of library names)
 )
 
-setup(name="fibonacci", version="1.0", description="...", ext_modules=[module1])
+setup(name="fibonacci", version="1.0", description="...", ext_modules=[module])
