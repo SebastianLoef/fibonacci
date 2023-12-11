651351e4e8de44615fe96ddadcaa433da77af974
SebastianLoef <sebastian@loef.se>
2023-12-11 16:33:45 +0100

    formatted code

--- fib/main.c
+++ fib/main.c
@@ -1,41 +1,40 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
-
 int fib_internal(int n)
 {
     int a = 0, b = 1, c = 0;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
         c = a + b;
         a = b;
         b = c;
     }
     return c;
 }
-static PyObject * my_c_module_fib(PyObject *self, PyObject *args)
+static PyObject *my_c_module_fib(PyObject *self, PyObject *args)
 {
     unsigned n;
     if (!PyArg_ParseTuple(args, "i", &n))
         return NULL;
 
     PyObject *out;
-    out = PyLong_FromLong(fib_internal(n));  
+    out = PyLong_FromLong(fib_internal(n));
     return out;
 }
 
 static PyMethodDef my_c_module_methods[] = {
-    {"fib",  my_c_module_fib, METH_VARARGS, "Returns a list with the first n Fibonacci numbers."},
-    {NULL, NULL, 0, NULL}        /* Sentinel */
+    {"fib", my_c_module_fib, METH_VARARGS, "Returns a list with the first n Fibonacci numbers."},
+    {NULL, NULL, 0, NULL} /* Sentinel */
 };
 
 static struct PyModuleDef my_c_module = {
     PyModuleDef_HEAD_INIT,
-    "my_c_module",   /* name of module */
+    "my_c_module",                 /* name of module */
     "My first Python module in C", /* module documentation, may be NULL */
-    -1,       /* size of per-interpreter state of the module,
-                 or -1 if the module keeps state in global variables. */
-    my_c_module_methods
-};
+    -1,                            /* size of per-interpreter state of the module,
+                                      or -1 if the module keeps state in global variables. */
+    my_c_module_methods};
 
 PyMODINIT_FUNC PyInit_my_c_module(void)
 {
--- test.py
+++ test.py
@@ -2,18 +2,22 @@ from my_c_module import fib
 import time
 from typing import Callable, Any
 
+
 def time_taken(func: Callable[[int], int], *args: Any):
     start = time.time()
     res = func(*args)
     time_taken = (time.time() - start) * 1000
     print(f"Time taken: {time_taken} ms")
     return res
+
+
 def fib_py(n: int) -> int:
-    a,b,c = 0,1,0
+    a, b, c = 0, 1, 0
     for _ in range(n):
-        c = a+b
-        a,b = b,c
+        c = a + b
+        a, b = b, c
     return c
+
 
 def main(n: int = 10000000):
     print(f"Calculating fibonnaci number n={n} using Python")
@@ -21,5 +25,6 @@ def main(n: int = 10000000):
     print(f"Calculating fibonnaci number n={n} using C")
     res = time_taken(fib, n)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
